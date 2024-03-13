b127ad577929366fc89548fcc0ce75069b0a7fb9
Henrique <henrique.nunesaguiar2007@gmail.com>
2024-03-13 08:33:02 -0300

    Criado arquivo para conceitos de variaveis

---
+++ aula01/funcies.js
@@ -0,0 +1,10 @@
+let num1 = 2
+const num2 = 3
+var msg = "Olá amigos! Esse é o número:"
+
+console.log (msg + num1)
+console.log (num1 + num2 )
+console.log ( typeof num1)
+console.log ( typeof msg)
+
+//contro + m 
