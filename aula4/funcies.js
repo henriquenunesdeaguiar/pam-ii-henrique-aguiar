d797c4b77b06121a93c2e938f00eae6890b41b7f
henriquenunesdeaguiar <henrique.nunesaguiar2007@gmail.com>
2024-05-08 08:31:09 -0300

    Aula 04

---
+++ aula4/funcies.js
@@ -0,0 +1,10 @@
+const TesteImparPar = (n) => {
+   let calculoTeste = n % 2
+
+return (calculoTeste == 1) ? "Número Impar" :  "Número Par"
+
+}
+
+console.log(TesteImparPar(17))
+console.log(TesteImparPar(64))
+console.log(TesteImparPar(1500))
